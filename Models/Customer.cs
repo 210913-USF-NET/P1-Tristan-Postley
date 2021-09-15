8c79c7127053387d58e17a2cee760482f4548578
Tristan Postley <tpostley@gmail.com>
2021-09-15 08:14:23 -0700

    Initialization

---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Models
+{
+    public class Customer
+    {
+        public Customer() {}
+
+        public Customer(string name) {
+
+        }
+
+        public string Name {get; set;}
+    }
+}
